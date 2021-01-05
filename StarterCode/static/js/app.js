fd0e83b2fb6fc282fc9cc9f44a8997f55ec4a01e
James-Ashley <jkbashley@gmail.com>
2021-01-05 09:54:11 -0500

    trying to render gauge

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -23,8 +23,8 @@ dropDown();
 function optionChanged(sample) {
     metaData(sample);
     chart(sample);
-
-}
+    constructGauge(sample);
+};
 
 function metaData(sampleID) {
     d3.json("samples.json").then((data) => {
@@ -34,17 +34,49 @@ function metaData(sampleID) {
         // for (var i = 0; i < metaIDs.length; i++) {
             // console.log(metaIDs[i])
             var filterID = metaIDs.filter(object => object.id == sampleID);
-            console.log(filterID);
+            // console.log(filterID);
             var results = filterID[0];
             var display = d3.select("#sample-metadata");
             display.html("");
             Object.entries(results).forEach(([key, value]) => {
                 display.append("h6").text(`${key} ${value}`);
-            })
-        // };
+            });
+            gauge(results.wfreq);
+
     });
 };
 
 function chart(sampleID) {
-    samples
-}
+        d3.json("samples.json").then((data) => {
+        var samples = data.samples; 
+        var filterID = samples.filter(object => object.id == sampleID);
+            console.log(filterID);
+        var results = filterID[0];
+        var otuIDs = results.otu_ids;
+        var sampleValues = results.sample_values;
+        var otuLabels = results.otu_labels;
+        var barData = [{
+            x: sampleValues.slice(0, 10).reverse(),
+            y: otuIDs.slice(0, 10).map(otu_ID => `otuID ${otu_ID}`).reverse(),
+            text: otuLabels.slice(0, 10).reverse(),
+            type: "bar", 
+            orientation: "h"
+        }];
+        Plotly.newPlot("bar", barData)
+        var bubbleData = [{
+            x: otuIDs,
+            y: sampleValues,
+            text: otuLabels,
+            mode: "markers",
+            marker: {
+                size: sampleValues,
+                color: otuIDs,
+                colorscale: "Earth" 
+            }
+        }];
+        Plotly.newPlot("bubble", bubbleData)
+    });
+
+
+        
+};
